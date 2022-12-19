e1f7467ca2ea99cfe7be41269f419c58d5c741a6
Edvvxrd <fedelis.edward@clsu2.edu.ph>
2022-12-19 23:44:39 +0800

    Upload Files

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/test.css">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+</head>
+<body class="to-blue">
+	<h1>Hello World</h1>
+	<h2>test</h2>
+<script type="text/javascript" src="js/test.js"></script>
+</body>
+</html>
